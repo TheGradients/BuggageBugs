f82a54fe8ef3420fee66d777a34cf8b7511d2475
Uttkarsh24 <uttkarshsingh1973@gmail.com>
2025-03-27 14:11:16 +0530

    Add user details management and update schema for unique fields

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import {Router} from 'express';
 import {auth} from 'express-openid-connect';
-import { login, logout, register } from '../controllers/user.controllers.js';
+import { login, logout, register ,addDetails } from '../controllers/user.controllers.js';
 import verifyJWT from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -8,5 +8,6 @@ const router = Router();
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/logout').post(verifyJWT,logout);
+router.route('/addDetails').post(verifyJWT,addDetails);
 
 export default router;
